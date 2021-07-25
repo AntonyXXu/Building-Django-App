4c74a72e2111e0ef914cb1e39d2551105772e415
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-07-25 15:21:12 -0600

    word counter

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -4,3 +4,11 @@ from django.http import HttpResponse
 def index(request):
     name = "abc"
     return render(request, "index.html", {"name": name})
+
+def counter(request):
+    words = request.GET['text']
+    length = len(words.split())
+    context = {
+        length: length
+    }
+    return render(request, "counter.html", context)
