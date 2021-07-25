31b3442d6ed872f4289eacb634a5d6a04a404a53
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-07-25 15:38:22 -0600

    code block django templating, data models

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Features
 # Create your views here.
 def index(request):
     name = "abc"
-    return render(request, "index.html", {"name": name})
+    f = Features()
+    f.id = 0
+    f.name = 'test'
+    f.details = "abcdefghijklmnop"
+
+    f1 = Features()
+    f1.id = 0
+    f1.name = 'test'
+    f1.details = "abcdefghijklmnop"
+
+    features = [f1, f2]
+    return render(request, "index.html", {'features': features})
 
 def counter(request):
     words = request.GET['text']
@@ -12,3 +24,4 @@ def counter(request):
         length: length
     }
     return render(request, "counter.html", context)
+
